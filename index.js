f893a35a0798524810c923a09c380a5f4f6d4a7a
sundana <firmansyahsundana@gmail.com>
2022-03-18 13:42:38 +0700

    Memberi fungsionalitas

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+/* Variabel global */
+
+const daftarPertanyaan = [
+    'Apa itu kebenaran',
+    'Apa itu pengetahuan',
+    'Apa itu keindahan',
+    'Apa itu kebahagiaan',
+    'Apa itu kenyataan',
+    'Apa itu kebaikan',
+    'Apa itu kejahatan',
+    'Apa yang membuat suatu tindakan yang baik',
+    'Kenapa kebaikan lebih cenderung dikejar dibandingkan kejahatan',
+    'Apa itu moralitas',
+    'Mengapa manusia harus bermoral',
+    'Apa manusia adalah tolok ukur segala sesuatu'
+]
+
+const acakPertanyaan = arr => {
+    const indeksAcak = Math.floor(Math.random() * arr.length)
+    let result = arr[indeksAcak] + '?'
+    return result
+}
+
+console.log(acakPertanyaan(daftarPertanyaan))
